2a96d1c10e9bb02066a839861dc8168046dcc36f
WcaleNieWolny <isupermichael007@gmail.com>
2026-09-25 18:42:37 +0200

    chore(replica): refresh schema snapshot

--- read_replicate/schema_replicate.sql
+++ read_replicate/schema_replicate.sql
@@ -892,20 +892,6 @@ CREATE INDEX idx_apps_onboarding_ota_stage ON public.apps USING btree (((((onboa
 
 
 --
--- Name: idx_apps_onboarding_queued_refresh_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_apps_onboarding_queued_refresh_at ON public.apps USING btree (COALESCE((onboarding ->> 'queued_refresh_at'::text), ''::text), COALESCE((onboarding ->> 'refreshed_at'::text), ''::text), app_id);
-
-
---
--- Name: idx_apps_onboarding_refreshed_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_apps_onboarding_refreshed_at ON public.apps USING btree (COALESCE((onboarding ->> 'refreshed_at'::text), ''::text), app_id);
-
-
---
 -- Name: idx_apps_owner_org_app_id; Type: INDEX; Schema: public; Owner: -
 --
 
